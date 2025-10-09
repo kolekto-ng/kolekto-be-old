bccaca4e5f48d6cf729befd8fcb288e2c098d814
abdullahi-mohammed <moh.abdullahi2003@gmail.com>
2025-10-09 12:34:28 +0100

    done

--- routes/admin/kyc.js
+++ routes/admin/kyc.js
@@ -5,19 +5,20 @@ import {
     getKycVerifications,
     getSingleKycVerification
 } from "../../controllers/admin/kyc.js";
+import verifyToken from "../../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Get all KYC submissions (all users)
-router.get("/kyc-documents", getAllKycDocuments);
+router.get("/kyc-documents", verifyToken, getAllKycDocuments);
 
 // Get all KYC documents/files for a specific user
-router.get("/kyc-documents/:userId", getUserKycDocuments);
+router.get("/kyc-documents/:userId", verifyToken, getUserKycDocuments);
 
 // Get all KYC verifications
-router.get("/kyc-verifications", getKycVerifications);
+router.get("/kyc-verifications", verifyToken, getKycVerifications);
 
 // Get a single KYC verification
-router.get("/kyc-verifications/:id", getSingleKycVerification);
+router.get("/kyc-verifications/:id", verifyToken, getSingleKycVerification);
 
 export default router;
